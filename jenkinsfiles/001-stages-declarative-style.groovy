70a14640108a8f5afd163bf5e9911d0146ea36c6
Aparajit Ghosh <Aparajit.Ghosh@INDECOMM.LOCAL>
2019-10-03 17:33:57 +0530

    Scripts Modified

--- jenkinsfiles/001-stages-declarative-style.groovy
+++ jenkinsfiles/001-stages-declarative-style.groovy
@@ -20,4 +20,9 @@ pipeline {
     }
 
   }
+  post {
+    always {
+        emailext body: 'A Test EMail', recipientProviders: [[$class: 'DevelopersRecipientProvider'], [$class: 'RequesterRecipientProvider']], subject: 'Test'
+    }
+}
 }
