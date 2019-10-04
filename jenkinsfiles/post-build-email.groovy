84635e7fcc55a78e7f4fd7fe16e9bedd08841cbd
Aparajit Ghosh <Aparajit.Ghosh@INDECOMM.LOCAL>
2019-10-04 15:35:08 +0530

    Receipents hardcoded for the testing

--- jenkinsfiles/post-build-email.groovy
+++ jenkinsfiles/post-build-email.groovy
@@ -1,39 +1,16 @@
 pipeline {
-    environment {
-        //This variable need be tested as string
-        doError = '1'
-    }
-   
     agent any
     
     stages {
-        stage('Error') {
-            when {
-                expression { doError == '1' }
-            }
+        stage('Ok') {
             steps {
-                echo "Failure"
-                error "failure test. It's work"
-            }
-        }
-        
-        stage('Success') {
-            when {
-                expression { doError == '0' }
-            }
-            steps {
-                echo "ok"
+                echo "Ok"
             }
         }
     }
     post {
         always {
-            echo 'I will always say Hello again!'
-            
-                emailext body: "${DEFAULT_CONTENT}",
-                recipientProviders: [[$class: 'DevelopersRecipientProvider'], [$class: 'RequesterRecipientProvider']],
-                subject: "${DEFAULT_SUBJECT}"
-            
+            emailext body: 'A Test EMail', recipientProviders: [[$class: 'DevelopersRecipientProvider'], [$class: 'RequesterRecipientProvider']], subject: 'Test'
         }
     }
 }
