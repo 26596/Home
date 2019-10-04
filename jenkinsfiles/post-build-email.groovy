cb06fe7fc216c2e5292e3d9d07a6e5c2c669f796
Aparajit Ghosh <Aparajit.Ghosh@INDECOMM.LOCAL>
2019-10-04 15:04:09 +0530

    Receipents hardcoded for the testing

--- jenkinsfiles/post-build-email.groovy
+++ jenkinsfiles/post-build-email.groovy
@@ -30,10 +30,10 @@ pipeline {
         always {
             echo 'I will always say Hello again!'
             
-            emailtext body: "${currentBuild.currentResult}: Job ${env.JOB_NAME} build ${env.BUILD_NUMBER}\n More info at: ${env.BUILD_URL}",
-                //recipientProviders: [[$class: 'DevelopersRecipientProvider'], [$class: 'RequesterRecipientProvider']],
-				recipientProviders: 'jagannath.singh@indecomm.net', 'er.aparajit@gmail.com'
-                subject: "Jenkins Build ${currentBuild.currentResult}: Job ${env.JOB_NAME}"
+                emailtext body: "${DEFAULT_CONTENT}",
+                recipientProviders: "${DEFAULT_RECIPIENTS}",
+                subject: "${DEFAULT_SUBJECT}"
+				
             
         }
     }
