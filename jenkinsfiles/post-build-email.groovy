667eb049648ffcf8a8223a472eb8f4300fee18b6
Aparajit Ghosh <Aparajit.Ghosh@INDECOMM.LOCAL>
2019-10-04 16:16:56 +0530

    Receipents hardcoded for the testing

--- jenkinsfiles/post-build-email.groovy
+++ jenkinsfiles/post-build-email.groovy
@@ -15,7 +15,12 @@ post {
             emailext body: "${currentBuild.currentResult}: Job ${env.JOB_NAME} build ${env.BUILD_NUMBER}\n More info at: ${env.BUILD_URL}",
                 recipientProviders: [[$class: 'DevelopersRecipientProvider'], [$class: 'RequesterRecipientProvider']],
                 subject: "Jenkins Build ${currentBuild.currentResult}: Job ${env.JOB_NAME}"
-            
+				
+				
+         
         }
+		success {
+      mail to: jagannath.singh@indecomm.net,er.aparajit@gmail.com, subject: ‘The Pipeline success :(‘
+    }
     }
 }
